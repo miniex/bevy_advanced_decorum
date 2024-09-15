cdc55d235625ab3b9703224c7c2311cfeea033c9
Han Damin <miniex@daminstudio.net>
2024-09-15 21:34:35 +0900

    feat: refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
 //! # Bevy Advanced Decorum
 //!
 //! A highly customizable window decoration plugin for the Bevy engine,inspired by tauri-plugin-decorum
-//!
-//!
+
+#![warn(missing_docs, clippy::doc_markdown, clippy::unwrap_or_default)]
+#![warn(clippy::undocumented_unsafe_blocks)]
+#![allow(clippy::type_complexity)]
+
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
 
 mod decorum;
 pub mod plugin;
 #[cfg(target_os = "macos")]
 mod traffic;
 
-pub use decorum::*;
-
-#[cfg(target_os = "macos")]
-#[macro_use]
-extern crate objc;
-
 pub mod prelude {
     pub use crate::plugin::DecorumPlugin;
 }
+
+pub use decorum::*;
